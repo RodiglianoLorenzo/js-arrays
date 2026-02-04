8064017f826bb4c748ae8ed3db303f9d5c3e5403
Lorenzo Rodigliano <rodiglianolorenzo@gmail.com>
2026-02-04 18:21:41 +0100

    fix exercise 2

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,7 +10,7 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = []
+const reversedTeachers = [];
 for (let i = teachers.length - 1; i>= 0; i--) {
  reversedTeachers.push(teachers[i]);
 }
@@ -18,11 +18,17 @@ console.log(reversedTeachers)
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = teachers.filter(teachers => teachers.length >= 5);
-console.log(longNames);
+const longNames = [];
+for (let i = 0; i < teachers.length; i++){
+  if (teachers[i].length >= 5){
+    longNames.push(teachers[i]);
+  }
+}
+
+console.log(longNames)
 
 
-// 3. Rimuovi 'Ed' dall'array teachers
+/* // 3. Rimuovi 'Ed' dall'array teachers
  teachers.splice(5, 1);
 console.log(teachers);
 
@@ -35,3 +41,4 @@ console.log(isFabioPresent);
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = teachers.join();
 console.log(teachersString);
+ */
