4397c869dd1238b6964ec02d6617770de3421e22
Lorenzo Rodigliano <rodiglianolorenzo@gmail.com>
2026-02-04 12:31:17 +0100

    complite exercise 2, 3, 4

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -13,12 +13,20 @@ const teachers = [
 const fourthTeacher = teachers[3];
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-
+teachers[4] = "Patrick"
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop()
+console.log(teachers);
+console.log(lastTeacher);
+
+
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
+console.log(teachers);
+
+console.log(firstTeacher);
+
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
