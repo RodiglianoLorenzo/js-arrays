5b52815bdfe90155f1ff72764259a544e3a8428e
Lorenzo Rodigliano <rodiglianolorenzo@gmail.com>
2026-02-04 13:59:04 +0100

    complte task 3

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -16,9 +16,13 @@ console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = teachers.filter(teachers => teachers.length >= 5);
+console.log(longNames);
+
 
 // 3. Rimuovi 'Ed' dall'array teachers
+ teachers.splice(5, 1);
+console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
