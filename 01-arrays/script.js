e6cc471a7d9a4116960a338bd89bcb857845748d
Lorenzo Rodigliano <rodiglianolorenzo@gmail.com>
2026-02-04 12:55:53 +0100

    complte task 8

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -16,11 +16,11 @@ console.log(teachers);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 teachers[4] = "Patrick"
+
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers.pop()
 console.log(teachers);
 console.log(lastTeacher);
-
 
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
@@ -45,4 +45,8 @@ console.log(lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = teachers.length === 0;
+console.log(isTeachersEmpty);
+
+
+
